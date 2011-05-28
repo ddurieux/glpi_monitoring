b4dbf6fa3f542a982be5458230e2e5d4b902a859
ddurieux <ddurieux@b9e781ca-6730-41a0-a14f-bf47ef40bcdd>
2011-05-28 22:10:01 +0000

    Fix SQL with relations between hosts, shotsgroups, contact, contactgroups
    git-svn-id: https://forge.indepnet.net/svn/monitoring/trunk@14 b9e781ca-6730-41a0-a14f-bf47ef40bcdd

--- install/mysql/plugin_monitoring-1.0.0-empty.sql
+++ install/mysql/plugin_monitoring-1.0.0-empty.sql
@@ -33,12 +33,109 @@ CREATE TABLE `glpi_plugin_monitoring_hosts` (
 
 
 
+DROP TABLE IF EXISTS `glpi_plugin_monitoring_hosts_hosts`;
+
+CREATE TABLE `glpi_plugin_monitoring_hosts_hosts` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `plugin_monitoring_hosts_id_1` int(11) NOT NULL DEFAULT '0',
+  `plugin_monitoring_hosts_id_2` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `unicity` (`plugin_monitoring_hosts_id_1`,`plugin_monitoring_hosts_id_2`),
+  KEY `plugin_monitoring_hosts_id_2` (`plugin_monitoring_hosts_id_2`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+
+
 DROP TABLE IF EXISTS `glpi_plugin_monitoring_hostgroups`;
 
 CREATE TABLE `glpi_plugin_monitoring_hostgroups` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) DEFAULT NULL,
-  `plugin_monitoring_hosts` text COLLATE utf8_unicode_ci,
-  `plugin_monitoring_hostgroups` text COLLATE utf8_unicode_ci,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+
+
+DROP TABLE IF EXISTS `glpi_plugin_monitoring_hosts_hostgroups`;
+
+CREATE TABLE `glpi_plugin_monitoring_hosts_hostgroups` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `plugin_monitoring_hosts_id` int(11) NOT NULL DEFAULT '0',
+  `plugin_monitoring_hostgroups_id` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `unicity` (`plugin_monitoring_hosts_id`,`plugin_monitoring_hostgroups_id`),
+  KEY `plugin_monitoring_hostgroups_id` (`plugin_monitoring_hostgroups_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+
+
+DROP TABLE IF EXISTS `glpi_plugin_monitoring_hostgroups_hostgroups`;
+
+CREATE TABLE `glpi_plugin_monitoring_hostgroups_hostgroups` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `plugin_monitoring_hostgroups_id_1` int(11) NOT NULL DEFAULT '0',
+  `plugin_monitoring_hostgroups_id_2` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `unicity` (`plugin_monitoring_hostgroups_id_1`,`plugin_monitoring_hostgroups_id_2`),
+  KEY `plugin_monitoring_hostgroups_id_2` (`plugin_monitoring_hostgroups_id_2`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+
+
+DROP TABLE IF EXISTS `glpi_plugin_monitoring_contacts`;
+
+CREATE TABLE `glpi_plugin_monitoring_contacts` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `users_id` int(11) NOT NULL DEFAULT '0',
+  `pager` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+
+
+DROP TABLE IF EXISTS `glpi_plugin_monitoring_hosts_contacts`;
+
+CREATE TABLE `glpi_plugin_monitoring_hosts_contacts` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `plugin_monitoring_hosts_id` int(11) NOT NULL DEFAULT '0',
+  `plugin_monitoring_contacts_id` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `unicity` (`plugin_monitoring_hosts_id`,`plugin_monitoring_contacts_id`),
+  KEY `plugin_monitoring_contacts_id` (`plugin_monitoring_contacts_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+
+
+DROP TABLE IF EXISTS `glpi_plugin_monitoring_contactgroups`;
+
+CREATE TABLE `glpi_plugin_monitoring_contactgroups` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+
+
+DROP TABLE IF EXISTS `glpi_plugin_monitoring_contacts_contactgroups`;
+
+CREATE TABLE `glpi_plugin_monitoring_contacts_contactgroups` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `plugin_monitoring_contacts_id` int(11) NOT NULL DEFAULT '0',
+  `plugin_monitoring_contactgroups_id` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `unicity` (`plugin_monitoring_contacts_id`,`plugin_monitoring_contactgroups_id`),
+  KEY `plugin_monitoring_contactgroups_id` (`plugin_monitoring_contactgroups_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+
+
+DROP TABLE IF EXISTS `glpi_plugin_monitoring_contactgroups_contactgroups`;
+
+CREATE TABLE `glpi_plugin_monitoring_contactgroups_contactgroups` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `plugin_monitoring_contactgroups_id_1` int(11) NOT NULL DEFAULT '0',
+  `plugin_monitoring_contactgroups_id_2` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `unicity` (`plugin_monitoring_contactgroups_id_1`,`plugin_monitoring_contactgroups_id_2`),
+  KEY `plugin_monitoring_contactgroups_id_2` (`plugin_monitoring_contactgroups_id_2`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
