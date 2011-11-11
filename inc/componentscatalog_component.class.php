dc4742c081736a42d68c1ef173df7480e5c3844e
ddurieux <ddurieux@b9e781ca-6730-41a0-a14f-bf47ef40bcdd>
2011-11-11 09:51:09 +0000

    Continue link between components catalog and components
    git-svn-id: https://forge.indepnet.net/svn/monitoring/trunk@147 b9e781ca-6730-41a0-a14f-bf47ef40bcdd

---
+++ inc/componentscatalog_component.class.php
@@ -0,0 +1,43 @@
+<?php
+
+/*
+   ----------------------------------------------------------------------
+   Monitoring plugin for GLPI
+   Copyright (C) 2010-2011 by the GLPI plugin monitoring Team.
+
+   https://forge.indepnet.net/projects/monitoring/
+   ----------------------------------------------------------------------
+
+   LICENSE
+
+   This file is part of Monitoring plugin for GLPI.
+
+   Monitoring plugin for GLPI is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 2 of the License, or
+   any later version.
+
+   Monitoring plugin for GLPI is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with Monitoring plugin for GLPI.  If not, see <http://www.gnu.org/licenses/>.
+
+   ------------------------------------------------------------------------
+   Original Author of file: David DURIEUX
+   Co-authors of file:
+   Purpose of file:
+   ----------------------------------------------------------------------
+ */
+
+if (!defined('GLPI_ROOT')) {
+   die("Sorry. You can't access directly to this file");
+}
+
+class PluginMonitoringComponentscatalog_Component extends CommonDBTM {
+   
+}
+
+?>
