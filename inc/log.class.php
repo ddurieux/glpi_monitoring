30c3b3f7362b1906b8b7e21b15d34509612717bc
David Durieux <d.durieux@siprossii.com>
2012-04-01 16:26:56 +0200

    Clean events. closes #3393

--- inc/log.class.php
+++ inc/log.class.php
@@ -98,7 +98,7 @@ class PluginMonitoringLog extends CommonDBTM {
       $pmConfig   = new PluginMonitoringConfig();
       
       $id_restart = 0;
-      $a_restarts = $this->find("`action`='restart'", "`id` DESC", 1);
+      $a_restarts = $pmLog->find("`action`='restart'", "`id` DESC", 1);
       if (count($a_restarts) > 0) {
          $a_restart = current($a_restarts);
          $id_restart = $a_restart['id'];
@@ -111,6 +111,12 @@ class PluginMonitoringLog extends CommonDBTM {
          $query .= " AND `id` < '".$id_restart."'";
       }
       $DB->query($query);
+      
+      // Clean too events
+      $query = "DELETE FROM `glpi_plugin_monitoring_serviceevents`
+         WHERE UNIX_TIMESTAMP(date) < UNIX_TIMESTAMP()-$secs";
+      $DB->query($query);
+      
       return true;
    }
 
