ac5c8ebf8931e2b652618c446b05e23eb8555331
David Durieux <d.durieux@siprossii.com>
2012-06-16 11:16:47 +0200

    Fix compatibility with 0.83

--- inc/contact_item.class.php
+++ inc/contact_item.class.php
@@ -174,7 +174,7 @@ class PluginMonitoringContact_Item extends CommonDBTM {
       }
       
       Html::openArrowMassives("contact_item_form$rand", true);
-      Html::closeArrowMassives('deleteitem', $LANG['buttons'][6]);
+      Html::closeArrowMassives(array('deleteitem'), $LANG['buttons'][6]);
       
       echo "</table>";
       
