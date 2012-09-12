96f82eda735614c92129fda89f0086579de507c9
David Durieux <d.durieux@siprossii.com>
2012-09-12 06:21:13 +0200

    Fix pager

--- inc/display.class.php
+++ inc/display.class.php
@@ -345,7 +345,12 @@ class PluginMonitoringDisplay extends CommonDBTM {
                 Search::getArrayUrlLink("link2",$_GET['link2']);
 
       $parameters = "sort=".$_GET['sort']."&amp;order=".$_GET['order'].$globallinkto;
-      Html::printPager($_GET['start'], $numrows, $CFG_GLPI['root_doc']."/plugins/monitoring/front/display.php", $parameters);
+      if (strstr($_SERVER['PHP_SELF'], "displayfix.php")) {
+         $parameters .= "&tab=".$_GET['tab'];
+         Html::printPager($_GET['start'], $numrows, $CFG_GLPI['root_doc']."/plugins/monitoring/front/displayfix.php", $parameters);
+      } else {
+         Html::printPager($_GET['start'], $numrows, $CFG_GLPI['root_doc']."/plugins/monitoring/front/display.php", $parameters);
+      }
 
       $limit = $numrows;
       if ($_SESSION["glpilist_limit"] < $numrows) {
