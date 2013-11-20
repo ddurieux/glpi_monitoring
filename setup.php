beee2d3876199333f1ef006d054232783ad9856f
Fred MOHIER <frederic.mohier@ipmfrance.com>
2013-11-20 08:04:04 +0100

    Set new version to allow update
    Avoid problems with database when updating ...

--- setup.php
+++ setup.php
@@ -40,7 +40,7 @@
    ------------------------------------------------------------------------
  */
 
-define ("PLUGIN_MONITORING_VERSION","0.84+1.1");
+define ("PLUGIN_MONITORING_VERSION","0.84+1.2");
 
 define('_MPDF_TEMP_PATH', GLPI_PLUGIN_DOC_DIR.'/monitoring/pdf/');
 
