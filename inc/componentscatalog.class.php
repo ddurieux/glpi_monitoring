6bed34af1298c4395f3c2215cd727ee7e35206b4
Fred MOHIER <frederic.mohier@ipmfrance.com>
2013-12-04 19:35:59 +0100

    Minor fixes ...

--- inc/componentscatalog.class.php
+++ inc/componentscatalog.class.php
@@ -494,7 +494,7 @@ class PluginMonitoringComponentscatalog extends CommonDropdown {
       $query = "SELECT `glpi_computers`.`name`, `glpi_computers`.`entities_id`, ".$pmComponentscatalog_Host->getTable().".* FROM `".$pmComponentscatalog_Host->getTable()."`
          LEFT JOIN `glpi_computers` ON `glpi_computers`.`id` = `".$pmComponentscatalog_Host->getTable()."`.`items_id`
          WHERE `plugin_monitoring_componentscalalog_id`='".$componentscatalogs_id."' AND `glpi_computers`.`entities_id` IN (".$_SESSION['glpiactiveentities_string'].")";
-      Toolbox::logInFile("pm", "query hosts - $query\n");
+      // Toolbox::logInFile("pm", "query hosts - $query\n");
       $result = $DB->query($query);
       while ($dataComponentscatalog_Host=$DB->fetch_array($result)) {
          $ressources = array();
@@ -502,7 +502,7 @@ class PluginMonitoringComponentscatalog extends CommonDropdown {
          $queryService = "SELECT * FROM `".$pmService->getTable()."`
             WHERE `plugin_monitoring_componentscatalogs_hosts_id`='".$dataComponentscatalog_Host['id']."'
                AND `entities_id` IN (".$_SESSION['glpiactiveentities_string'].")";
-         Toolbox::logInFile("pm", "query services - $queryService\n");
+         // Toolbox::logInFile("pm", "query services - $queryService\n");
          $resultService = $DB->query($queryService);
          while ($dataService=$DB->fetch_array($resultService)) {
             $nb_ressources++;
