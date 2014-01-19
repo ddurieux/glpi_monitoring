bcc171301e89744ca72f989d33fc8ea3aa9abf93
Fred MOHIER <frederic.mohier@ipmfrance.com>
2014-01-19 12:05:54 +0100

    Fix merge problems ...
    ... do not know why but some merging failed !

--- front/menu.php
+++ front/menu.php
@@ -104,25 +104,26 @@ if (PluginMonitoringProfile::haveRight("config_services_catalogs", 'r')
    echo "<tr class='tab_bg_1'>";
    if (PluginMonitoringProfile::haveRight("config_services_catalogs", 'r')) {
       $toDisplayArea++;
+      echo "<th align='center' height='50' width='33%'>";
       echo "<a href='".$CFG_GLPI['root_doc']."/plugins/monitoring/front/servicescatalog.php'>".__('Services catalogs', 'monitoring')."</a>";
       echo "</th>";
    }
 
    if (PluginMonitoringProfile::haveRight("config_weathermap", 'r')) {
       $toDisplayArea++;
+      echo "<th align='center' height='50' width='33%'>";
       echo "<a href='".$CFG_GLPI['root_doc']."/plugins/monitoring/front/weathermap.php'>".__('Weathermaps', 'monitoring')."</a>";
       echo "</th>";
    }
 
+   if (PluginMonitoringProfile::haveRight("config_views", 'r')) {
       $toDisplayArea++;
-      echo "<th align='center' height='40' width='34%'>";
-      echo "<a href='".$CFG_GLPI['root_doc']."/plugins/monitoring/front/customitem_gauge.php'>".PluginMonitoringCustomitem_Gauge::getTypeName()."</a>";
+      echo "<th align='center' height='50' width='34%'>";
+      echo "<a href='".$CFG_GLPI['root_doc']."/plugins/monitoring/front/displayview.php'>".__('Views', 'monitoring')."</a>";
       echo "</th>";
    }
-   
    echo "</tr>";
    echo "</table>";
-
    echo "<br/>";
 }
 
