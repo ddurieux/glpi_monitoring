498f1a18147607a647f0ec7e44fea20ac191b944
Fred MOHIER <frederic.mohier@ipmfrance.com>
2014-02-07 08:34:37 +0100

    Set up new version 0.84+1.3d

--- setup.php
+++ setup.php
@@ -40,7 +40,7 @@
    ------------------------------------------------------------------------
  */
 
-define ("PLUGIN_MONITORING_VERSION","0.84+1.3c");
+define ("PLUGIN_MONITORING_VERSION","0.84+1.3d");
 
 define('_MPDF_TEMP_PATH', GLPI_PLUGIN_DOC_DIR.'/monitoring/pdf/');
 
