9fe4e689b71bdbb86e2e4e5176a7822f0bd037a7
David Durieux <d.durieux@siprossii.com>
2014-03-08 00:08:38 +0100

    Clean white spaces

--- inc/webservice.class.php
+++ inc/webservice.class.php
@@ -29,14 +29,14 @@
 
    @package   Plugin Monitoring for GLPI
    @author    David Durieux
-   @co-author 
-   @comment   
+   @co-author
+   @comment
    @copyright Copyright (c) 2011-2014 Plugin Monitoring for GLPI team
    @license   AGPL License 3.0 or (at your option) any later version
               http://www.gnu.org/licenses/agpl-3.0-standalone.html
    @link      https://forge.indepnet.net/projects/monitoring/
    @since     2011
- 
+
    ------------------------------------------------------------------------
  */
 
@@ -53,14 +53,14 @@ class PluginMonitoringWebservice {
          return array('file'  => 'config filename to get : commands.cfg, hosts.cfg',
                       'help'    => 'bool,optional');
       }
-      
+
       if (!isset($params['tag'])) {
          $params['tag'] = '';
       }
-      
+
       ini_set("max_execution_time", "0");
       ini_set("memory_limit", "-1");
-      
+
       $pmShinken = new PluginMonitoringShinken();
       switch ($params['file']) {
 
@@ -88,12 +88,12 @@ class PluginMonitoringWebservice {
             $array = $pmShinken->generateTimeperiodsCfg(1);
             return array($array[0]=>$array[1]);
             break;
-         
+
          case 'services.cfg':
             $array = $pmShinken->generateServicesCfg(1, $params['tag']);
             return array($array[0]=>$array[1]);
             break;
-         
+
          case 'templates.cfg':
             $array = $pmShinken->generateTemplatesCfg(1, $params['tag']);
             return array($array[0]=>$array[1]);
@@ -121,41 +121,41 @@ class PluginMonitoringWebservice {
       }
    }
 
-   
+
 
    static function methodShinkenCommands($params, $protocol) {
-      
+
       $pmShinken = new PluginMonitoringShinken();
       $array = $pmShinken->generateCommandsCfg();
       return $array;
    }
 
-   
-   
+
+
    static function methodShinkenHosts($params, $protocol) {
       if (!isset($params['tag'])) {
          $params['tag'] = '';
       }
-      
+
       // Update ip with Tag
       if (isset($_SERVER['REMOTE_ADDR'])) {
          $pmTag = new PluginMonitoringTag();
          $pmTag->setIP($params['tag'], $_SERVER['REMOTE_ADDR']);
       }
-      
+
       $pmShinken = new PluginMonitoringShinken();
       $array = $pmShinken->generateHostsCfg(0, $params['tag']);
       return $array;
    }
-   
-   
-   
+
+
+
    static function methodShinkenHostgroups($params, $protocol) {
 
       if (!isset($params['tag'])) {
          $params['tag'] = '';
       }
-      
+
       $pmShinken = new PluginMonitoringShinken();
       $array = $pmShinken->generateHostgroupsCfg(0, $params['tag']);
       return $array;
@@ -168,7 +168,7 @@ class PluginMonitoringWebservice {
       if (!isset($params['tag'])) {
          $params['tag'] = '';
       }
-      
+
       $pmShinken = new PluginMonitoringShinken();
       $array = $pmShinken->generateServicesCfg(0, $params['tag']);
       return $array;
@@ -181,14 +181,14 @@ class PluginMonitoringWebservice {
       if (!isset($params['tag'])) {
          $params['tag'] = '';
       }
-      
+
       $pmShinken = new PluginMonitoringShinken();
       $array = $pmShinken->generateTemplatesCfg(0, $params['tag']);
       return $array;
    }
-   
-   
-   
+
+
+
    static function methodShinkenContacts($params, $protocol) {
 
       $pmShinken = new PluginMonitoringShinken();
@@ -204,16 +204,16 @@ class PluginMonitoringWebservice {
       $array = $pmShinken->generateTimeperiodsCfg();
       return $array;
    }
-   
-   
-   
+
+
+
    static function methodDashboard($params, $protocol) {
       $response = array();
-      
+
       if (!isset($params['view'])) {
          return $response;
       }
-      
+
       $pm = new PluginMonitoringDisplay();
       if ($params['view'] == 'Hosts') {
          return $pm->displayHostsCounters(0);
@@ -221,27 +221,27 @@ class PluginMonitoringWebservice {
          return $pm->displayCounters($params['view'], 0);
       }
    }
-   
-   
-   
+
+
+
    static function methodGetServicesList($params, $protocol) {
       $array = PluginMonitoringWebservice::getServicesList($params['statetype'], $params['view']);
-      
+
       return $array;
    }
    static function getServicesList($statetype, $view) {
       global $DB;
-      
+
       $services = array();
-      
+
       if ($view == 'Ressources') {
-         
+
          switch ($statetype) {
-            
+
             case "ok":
                $query = "SELECT * FROM `glpi_plugin_monitoring_services`
-                  LEFT JOIN `glpi_plugin_monitoring_componentscatalogs_hosts` 
-                     ON `plugin_monitoring_componentscatalogs_hosts_id`= 
+                  LEFT JOIN `glpi_plugin_monitoring_componentscatalogs_hosts`
+                     ON `plugin_monitoring_componentscatalogs_hosts_id`=
                         `glpi_plugin_monitoring_componentscatalogs_hosts`.`id`
                   WHERE (`state`='OK' OR `state`='UP') AND `state_type`='HARD'";
                $result = $DB->query($query);
@@ -249,15 +249,15 @@ class PluginMonitoringWebservice {
                   $itemtype = $data['itemtype'];
                   $item = new $itemtype();
                   $item->getFromDB($data['items_id']);
-                  
+
                   $services[] = "(".$itemtype.") ".$item->getName()."\n=> ".$data['name'];
                }
                break;
 
             case "warning":
                $query = "SELECT * FROM `glpi_plugin_monitoring_services`
-                  LEFT JOIN `glpi_plugin_monitoring_componentscatalogs_hosts` 
-                     ON `plugin_monitoring_componentscatalogs_hosts_id`= 
+                  LEFT JOIN `glpi_plugin_monitoring_componentscatalogs_hosts`
+                     ON `plugin_monitoring_componentscatalogs_hosts_id`=
                         `glpi_plugin_monitoring_componentscatalogs_hosts`.`id`
                   WHERE (`state`='WARNING' OR `state`='UNKNOWN' OR `state`='RECOVERY' OR `state`='FLAPPING' OR `state` IS NULL)
                     AND `state_type`='HARD'";
@@ -266,15 +266,15 @@ class PluginMonitoringWebservice {
                   $itemtype = $data['itemtype'];
                   $item = new $itemtype();
                   $item->getFromDB($data['items_id']);
-                  
+
                   $services[] = "(".$itemtype.") ".$item->getName()."\n=> ".$data['name'];
                }
                break;
-            
+
             case "critical":
                $query = "SELECT * FROM `glpi_plugin_monitoring_services`
-                  LEFT JOIN `glpi_plugin_monitoring_componentscatalogs_hosts` 
-                     ON `plugin_monitoring_componentscatalogs_hosts_id`= 
+                  LEFT JOIN `glpi_plugin_monitoring_componentscatalogs_hosts`
+                     ON `plugin_monitoring_componentscatalogs_hosts_id`=
                         `glpi_plugin_monitoring_componentscatalogs_hosts`.`id`
                   WHERE (`state`='DOWN' OR `state`='UNREACHABLE' OR `state`='CRITICAL' OR `state`='DOWNTIME')
                     AND `state_type`='HARD'";
@@ -283,17 +283,17 @@ class PluginMonitoringWebservice {
                   $itemtype = $data['itemtype'];
                   $item = new $itemtype();
                   $item->getFromDB($data['items_id']);
-                  
+
                   $services[] = "(".$itemtype.") ".$item->getName()."\n=> ".$data['name'];
                }
                break;
          }
-         
+
       } else if ($view == 'Componentscatalog') {
          $pmComponentscatalog_Host = new PluginMonitoringComponentscatalog_Host();
          $queryCat = "SELECT * FROM `glpi_plugin_monitoring_componentscatalogs`";
          $resultCat = $DB->query($queryCat);
-         while ($data=$DB->fetch_array($resultCat)) { 
+         while ($data=$DB->fetch_array($resultCat)) {
 
             $query = "SELECT * FROM `".$pmComponentscatalog_Host->getTable()."`
                WHERE `plugin_monitoring_componentscalalog_id`='".$data['id']."'";
@@ -302,19 +302,19 @@ class PluginMonitoringWebservice {
             $state['ok'] = 0;
             $state['warning'] = 0;
             $state['critical'] = 0;
-            while ($dataComponentscatalog_Host=$DB->fetch_array($result)) {            
+            while ($dataComponentscatalog_Host=$DB->fetch_array($result)) {
 
-               $state['ok'] += countElementsInTable("glpi_plugin_monitoring_services", 
+               $state['ok'] += countElementsInTable("glpi_plugin_monitoring_services",
                        "(`state`='OK' OR `state`='UP') AND `state_type`='HARD'
                           AND `plugin_monitoring_componentscatalogs_hosts_id`='".$dataComponentscatalog_Host['id']."'");
 
 
-               $state['warning'] += countElementsInTable("glpi_plugin_monitoring_services", 
+               $state['warning'] += countElementsInTable("glpi_plugin_monitoring_services",
                        "(`state`='WARNING' OR `state`='UNKNOWN' OR `state`='RECOVERY' OR `state`='FLAPPING' OR `state` IS NULL)
                           AND `state_type`='HARD'
                           AND `plugin_monitoring_componentscatalogs_hosts_id`='".$dataComponentscatalog_Host['id']."'");
 
-               $state['critical'] += countElementsInTable("glpi_plugin_monitoring_services", 
+               $state['critical'] += countElementsInTable("glpi_plugin_monitoring_services",
                        "(`state`='DOWN' OR `state`='UNREACHABLE' OR `state`='CRITICAL' OR `state`='DOWNTIME')
                           AND `state_type`='HARD'
                           AND `plugin_monitoring_componentscatalogs_hosts_id`='".$dataComponentscatalog_Host['id']."'");
@@ -335,7 +335,7 @@ class PluginMonitoringWebservice {
             }
          }
       } else if ($view == 'Businessrules') {
-         
+
       }
       return $services;
    }
@@ -347,7 +347,7 @@ class PluginMonitoringWebservice {
    }
    static function getHostsStates($filter) {
       global $DB;
-      
+
       $hosts = array();
 
       $query = "
@@ -395,7 +395,7 @@ class PluginMonitoringWebservice {
    }
    static function getServicesStates($filter) {
       global $DB;
-      
+
       $hosts = array();
 
       $query = "
@@ -408,20 +408,20 @@ class PluginMonitoringWebservice {
             `glpi_plugin_monitoring_componentscatalogs_hosts`.*,
             `glpi_plugin_monitoring_components`.*
          FROM `glpi_plugin_monitoring_componentscatalogs_hosts`
-         INNER JOIN `glpi_plugin_monitoring_services` 
+         INNER JOIN `glpi_plugin_monitoring_services`
             ON (`glpi_plugin_monitoring_services`.`plugin_monitoring_componentscatalogs_hosts_id` = `glpi_plugin_monitoring_componentscatalogs_hosts`.`id`)
-         INNER JOIN `glpi_plugin_monitoring_hosts` 
+         INNER JOIN `glpi_plugin_monitoring_hosts`
             ON `glpi_plugin_monitoring_componentscatalogs_hosts`.`items_id` = `glpi_plugin_monitoring_hosts`.`items_id` AND `glpi_plugin_monitoring_componentscatalogs_hosts`.`itemtype` = `glpi_plugin_monitoring_hosts`.`itemtype`
-         INNER JOIN `glpi_plugin_monitoring_componentscatalogs` 
+         INNER JOIN `glpi_plugin_monitoring_componentscatalogs`
             ON `plugin_monitoring_componentscalalog_id` = `glpi_plugin_monitoring_componentscatalogs`.`id`
-         INNER JOIN `glpi_plugin_monitoring_components` 
+         INNER JOIN `glpi_plugin_monitoring_components`
             ON (`glpi_plugin_monitoring_services`.`plugin_monitoring_components_id` = `glpi_plugin_monitoring_components`.`id`)
-         INNER JOIN `glpi_entities` 
+         INNER JOIN `glpi_entities`
             ON (`glpi_plugin_monitoring_services`.`entities_id` = `glpi_entities`.`id`)
          LEFT JOIN `glpi_computers`
-            ON `glpi_plugin_monitoring_componentscatalogs_hosts`.`items_id` = `glpi_computers`.`id` AND `glpi_plugin_monitoring_componentscatalogs_hosts`.`itemtype`='Computer' 
+            ON `glpi_plugin_monitoring_componentscatalogs_hosts`.`items_id` = `glpi_computers`.`id` AND `glpi_plugin_monitoring_componentscatalogs_hosts`.`itemtype`='Computer'
          LEFT JOIN `glpi_printers`
-            ON `glpi_plugin_monitoring_componentscatalogs_hosts`.`items_id` = `glpi_printers`.`id` AND `glpi_plugin_monitoring_componentscatalogs_hosts`.`itemtype`='Printer' 
+            ON `glpi_plugin_monitoring_componentscatalogs_hosts`.`items_id` = `glpi_printers`.`id` AND `glpi_plugin_monitoring_componentscatalogs_hosts`.`itemtype`='Printer'
          LEFT JOIN `glpi_networkequipments`
             ON `glpi_plugin_monitoring_componentscatalogs_hosts`.`items_id` = `glpi_networkequipments`.`id` AND `glpi_plugin_monitoring_componentscatalogs_hosts`.`itemtype`='NetworkEquipment'
       ";
