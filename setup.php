3b03e5ef28f901ee3550342ad29f906e33488554
David Durieux <d.durieux@siprossii.com>
2014-03-25 23:53:01 +0100

    Update version

--- setup.php
+++ setup.php
@@ -40,7 +40,7 @@
    ------------------------------------------------------------------------
  */
 
-define ("PLUGIN_MONITORING_VERSION","0.84+1.3f");
+define ("PLUGIN_MONITORING_VERSION","0.84+1.3g");
 
 define('_MPDF_TEMP_PATH', GLPI_PLUGIN_DOC_DIR.'/monitoring/pdf/');
 
