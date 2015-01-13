b994045148f9e3e635658f68965367e2c83327aa
Frédéric MOHIER <frederic.mohier@ipmfrance.com>
2015-01-13 10:46:56 +0100

    Fix error for Shinken acknowledges and downtimes

--- inc/shinkenwebservice.class.php
+++ inc/shinkenwebservice.class.php
@@ -75,9 +75,9 @@ class PluginMonitoringShinkenwebservice extends CommonDBTM {
       }
       $ip = $pmTag->getIP($tag);
       $auth = $pmTag->getAuth($tag);
+      $port = $pmTag->getPort($tag);
 
-
-      $url = 'http://'.$ip.':7760/';
+      $url = 'http://'.$ip.':'.$port.'/';
       $action = 'acknowledge';
       $a_fields = array(
           'action'               => empty($operation) ? 'add' : $operation,
@@ -120,8 +120,9 @@ class PluginMonitoringShinkenwebservice extends CommonDBTM {
       }
       $ip = $pmTag->getIP($tag);
       $auth = $pmTag->getAuth($tag);
+      $port = $pmTag->getPort($tag);
 
-      $url = 'http://'.$ip.':7760/';
+      $url = 'http://'.$ip.':'.$port.'/';
       $action = 'downtime';
       $a_fields = array(
           'action'               => empty($operation) ? 'add' : $operation,
