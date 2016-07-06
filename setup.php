f61c93c57d8763a38bd4ec491d689c0a0f172f25
Frédéric MOHIER <frederic.mohier@gmail.com>
2016-07-06 18:49:24 +0200

    Add missing javascript external functions

--- setup.php
+++ setup.php
@@ -116,6 +116,7 @@ function plugin_init_monitoring() {
              "lib/jqueryplugins/tagbox/js/jquery.tagbox.min.js",
              "lib/webui/bootstrap.min.js",
              "lib/webui/datatables.min.js",
+             "lib/webui/alignak_webui-external.js"
              "lib/webui/Chart.min.js"
              );
 
