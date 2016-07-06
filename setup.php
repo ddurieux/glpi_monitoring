af50dbfedf115cfedd7d61d7267a32c0f22ee2c8
Frédéric MOHIER <frederic.mohier@gmail.com>
2016-07-06 18:02:16 +0200

    Include Chart.min.js library for external widgets

--- setup.php
+++ setup.php
@@ -116,7 +116,7 @@ function plugin_init_monitoring() {
              "lib/jqueryplugins/tagbox/js/jquery.tagbox.min.js",
              "lib/webui/bootstrap.min.js",
              "lib/webui/datatables.min.js",
-             "lib/webui/plugins/chart.min.js"
+             "lib/webui/plugins/Chart.min.js"
              );
 
          $plugin = new Plugin();
